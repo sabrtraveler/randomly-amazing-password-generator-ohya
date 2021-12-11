97af196510e4c5e6476e2665fc312714fa17e73c
sabrtraveler <sabrtraveler@gmail.com>
2021-12-10 22:10:30 -0500

    did some pseudocode

--- Develop/script.js
+++ Develop/script.js
@@ -1,6 +1,25 @@
+//Ask user what type of pass they want
+// a. how long do you want the pass to be? Prompt -> depending on  response -> set password length
+// -> if statement
+// -> store the requested length into Password Object
+
+// b. Do you want lowercase letters in your pass? -> depending on  response -> set lowercase
+// -> if statement
+// -> store the requested answer into Password Object
+
+// c. Do you want uppercase letters in your pass? -> depending on  response -> set uppercase 
+// -> if statement
+// -> store the requested answer into Password Object
+
+// d. Do you want numbers in your pass? -> depending on  response -> set numbers
+// -> if statement
+// -> store the requested answer into Password Object
+
+// e. Do you want special characters in your pass? -> depending on  response -> set special characters
+// -> if statement
+// -> store the requested answer into Password Object
+
 // Assignment code here
-
-
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
@@ -15,3 +34,9 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+
+
+
+
+ 
